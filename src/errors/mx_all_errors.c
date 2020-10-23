503a712e0cec697b052f5bf0a3ac9d0b34602307
lyefimov <lyefimov@student.ucode.world>
2020-10-24 01:23:32 +0300

    invalid_n.c

--- src/errors/mx_all_errors.c
+++ src/errors/mx_all_errors.c
@@ -1,42 +1,4 @@
 #include "pathfinder.h"
-
-static int smth(struct file0 *data, int i) {
-    int j = 0;
-    int num = 1;
-    int tire = 0;
-    int word1 = 1;
-    int word2 = 1;
-
-    for (; mx_isalpha(data->file_lines[i][j]); j++)
-        word1 = 0;
-    (data->file_lines[i][j++] != '-') ? tire = 1 : 1;
-    for (; mx_isalpha(data->file_lines[i][j]); j++)
-        word2 = 0;
-    (data->file_lines[i][j++] != ',') ? tire = 1: 1;
-    for (; mx_isdigit(data->file_lines[i][j]); j++)
-        num = 0;
-    (data->file_lines[i][j] != '\0') ? tire = 1 : 1;
-    if (tire + word1 + num + word2 > 0)
-        return 1;
-    return 0;
-}
-
-bool invalid_n(struct file0 *data) {
-    char *line = 0;
-    //long a = 0;
-
-    for (int i = 1; i < mx_count_words(data->file, '\n'); i++)
-        if (smth(data, i)) {
-        line = mx_itoa(i + 1);
-        mx_printerr("error: line ");
-        mx_printerr(line);
-        mx_printerr(" is not valid\n");
-        free(line);
-        return 1;
-        }
-    return 0;
-}
-
 /*bool duplicates(char *argv[]) {
 
 
